b0572a7710204821750246a04901bf39fa3ef32e
vitaliy-kro <krochakv6@gmail.com>
2022-11-06 01:22:02 +0200

    fixed theme

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
+import theme from 'theme';
 import './index.css';
-import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
