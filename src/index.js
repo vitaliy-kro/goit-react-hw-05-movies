f074c41ce403e85d7e7d747de022dc36f1a55ad1
vitaliy-kro <krochakv6@gmail.com>
2022-11-06 01:11:34 +0200

    add / in BrowseRouter

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
+      <ThemeProvider theme={theme}>
         <App />
-      </BrowserRouter>
-    </ThemeProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
